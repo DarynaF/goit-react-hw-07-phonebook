1722d874d0353f0023fbb2cd17680d8a36900a7d
DarynaF <karlu4ka333@gmail.com>
2023-03-07 13:21:46 +0300

    1

---
+++ src/shared/contacts.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const contactsInstance = axios.create({
+  baseURL: 'https://640510f2eed195a99f7ba05b.mockapi.io/contacts',
+});
+
+export const getAllContacts = async () => {
+  const { data } = await contactsInstance.get('/');
+  return data;
+};
+
+export const addContact = async data => {
+  const { data: result } = await contactsInstance.post('./', data);
+  return result;
+};
+
+export const deleteContact = async id => {
+  const { data } = await contactsInstance.delete(`./${id}`);
+  return data;
+};
+
